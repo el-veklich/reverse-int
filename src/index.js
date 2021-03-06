8eb39a490e64cf64518db47c5b10038b17733666
el-veklich <dr.leonid.veklich@gmail.com>
2021-03-06 17:11:14 +0300

    solved: reverse-int problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return Number(
+            String(-1 * n)
+                .split("")
+                .reverse()
+                .join("")
+        );
+    } else {
+        return Number(String(n).split("").reverse().join(""));
+    }
+};
